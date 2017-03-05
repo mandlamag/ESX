cb969c3afaca864bfa861ebab537b900efd22162
Mandla <mandla.magagula@gmail.com>
2017-03-05 20:41:08 +0200

    Add crash analytics to app

--- EXApp/android/app/src/main/java/com/exapp/MainApplication.java
+++ EXApp/android/app/src/main/java/com/exapp/MainApplication.java
@@ -3,6 +3,8 @@ package com.exapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.azure.mobile.react.crashes.RNCrashesPackage;
+import com.microsoft.azure.mobile.react.analytics.RNAnalyticsPackage;
 import com.auth0.lock.react.LockReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCrashesPackage(MainApplication.this, getResources().getString(R.string.mobileCenterCrashes_whenToSendCrashes)),
+            new RNAnalyticsPackage(MainApplication.this, getResources().getString(R.string.mobileCenterAnalytics_whenToEnableAnalytics)),
             new LockReactPackage()
       );
     }
